94e8eb08045aa0dd7b47513aeb5b04528fd72ff0
Jack Grahl <jack.grahl@gmail.com>
2018-05-12 22:04:12 +0000

    Portal effects.

--- window.py
+++ window.py
@@ -8,17 +8,19 @@ class Window(object):
     def __init__(self):
         # Create the window/Initialise
         self.dimensions = IMAGE_DIMENSIONS
+        self.center = (self.dimensions[0] // 2, self.dimensions[1] // 2)
         self.scaled_dimensions = WINDOW_DIMENSIONS
         self.real_window = pygame.display.set_mode(self.scaled_dimensions)
         self.window = pygame.Surface(self.dimensions)
         self.clock = pygame.time.Clock()
+        self.portal_offset = 0
         self.init_rex()
         self.draw()
 
     def init_rex(self):
-        self.rex_location = (self.dimensions[0] / 2, self.dimensions[1] / 2)
+        self.rex_location = self.center
         self.rex_image = pygame.image.load("img/REX.png")
-        self.rex_shift = (self.rex_image.get_width() / 2, self.rex_image.get_height() / 2)
+        self.rex_shift = (self.rex_image.get_width() // 2, self.rex_image.get_height() // 2)
     
     def main(self):
         self.clock.tick(40)
@@ -38,8 +40,13 @@ class Window(object):
         self.window.blit(self.rex_image, location)
     
     def draw_background(self):
+        WIDTH = 20
         self.window.fill((0, 0, 0))
-    
+        for i in range(1, self.dimensions[0] // (2 * WIDTH) + 2):
+            r = self.portal_offset + i * 2 * WIDTH
+            pygame.draw.circle(self.window, (128, 128, 128), self.center, r, WIDTH)
+        self.portal_offset = (self.portal_offset + 1) % (2 * WIDTH)
+
     def handle_key(self, key):
         pass
 
